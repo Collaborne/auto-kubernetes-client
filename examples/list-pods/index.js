aedf22e8cddea52713033d007869b493c7472476
Andreas Kohn <andreas.kohn@gmail.com>
2017-03-29 15:42:57 +0200

    Refactor the API to use promises and streams instead of callbacks

--- examples/list-pods/index.js
+++ examples/list-pods/index.js
@@ -13,24 +13,12 @@ const config = {
 	key: fs.readFileSync(path.resolve(userDir, '.minikube/apiserver.key'), 'UTF-8')
 };
 
-K8sClient(config, function(err, client) {
-	if (err) {
-		console.error(`Cannot connect to cluster: ${err.message}`);
-		return;
-	}
-
-	client.namespaces.list(function(err, response, nsList) {
-		if (err) {
-			console.error(`Cannot list namespaces: ${err.message}`);
-			return;
-		}
-
-		return nsList.items.forEach(function(ns) {
-			return client.ns(ns.metadata.name).pods.list(function(err, response, podList) {
-				podList.items.forEach(function(pod) {
-					console.log(`Discovered pod ${pod.metadata.namespace}/${pod.metadata.name}`);
-				});
-			});
-		});
-	});
+K8sClient(config).then(function(client) {
+	return client.namespaces.list()
+		.then(nsList => nsList.items.map(ns => client.ns(ns.metadata.name).pods.list()))
+		.then(podListPromises => Promise.all(podListPromises))
+		.then(podLists => podLists.reduce((result, podList) => result.concat(podList.items), []))
+		.then(pods => pods.forEach(pod => console.log(`Discovered pod ${pod.metadata.namespace}/${pod.metadata.name}`)));
+}).catch(function(err) {
+	console.error(`Error: ${err.message}`);
 });
